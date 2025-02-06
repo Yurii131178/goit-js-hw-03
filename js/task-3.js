5ff32f5671357b9d4a067825274f0134ffae95ca
YURII KO <yurii.kosenko.uniplyt@gmail.com>
2025-02-06 16:39:39 +0200

    t-3 done

--- js/task-3.js
+++ js/task-3.js
@@ -1,51 +1,16 @@
-
-// Напиши функцію filterArray(numbers, value), яка приймає масив чисел (numbers) та значення (value) як параметри. Функція повинна повертати новий масив лише тих чисел із масиву numbers, які більші за значення value.
-
-
-
-// Усередині функції:
-
-// Створи порожній масив, у який будеш додавати підходящі числа.
-// Використай цикл для ітерації кожного елемента масиву numbers.
-// Використовуй умовний оператор if усередині циклу для перевірки кожного елемента и додавання до свого масиву.
-// Поверни свій новий масив з підходящими числами як результат.
-
-
-
 function filterArray(numbers, value) {
-    const newArray = 0;
+  const newArray = [];
 
-    for (const num of numbers) {
-        if(num > value) {
-        newArray += num;
-        }
-        return newArray;
+  for (const num of numbers) {
+    if (num > value) {
+        newArray.push(num);
     }
-    
-
+  }
+  return newArray;
 }
 
-
-
-
 console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-// console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-// console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-// console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-// console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
-
-// Залиш цей код для перевірки ментором.
-
-
-
-// На що буде звертати увагу ментор при перевірці:
-
-
-
-// Оголошена функція filterArray(numbers, value)
-// Виклик функції filterArray([1, 2, 3, 4, 5], 3) повертає [4, 5]
-// Виклик функції filterArray([1, 2, 3, 4, 5], 4) повертає [5]
-// Виклик функції filterArray([1, 2, 3, 4, 5], 5) повертає []
-// Виклик функції filterArray([12, 24, 8, 41, 76], 38) повертає [41, 76]
-// Виклик функції filterArray([12, 24, 8, 41, 76], 20) повертає [24, 41, 76]
-// Виклик функції filterArray() з випадковим масивом і числом повертає правильний масив
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
